fd08c9d4a9ecd8db5638232b7a724114ebf94a65
alcpereira <48070464+alcpereira@users.noreply.github.com>
2022-07-05 16:50:32 +0100

    If nest destroyed
    Removed the if nesting and replaced it with a `continue` solution
    Added constant for new stroke width

--- CLO3DCleaning.jsx
+++ CLO3DCleaning.jsx
@@ -1,5 +1,5 @@
 var documentGroupItems = app.activeDocument.groupItems
-
+const NEW_STROKE_WIDTH = 1
 var clipGroups = Array()
 // Separated array as `app.activeDocument.groupItems` will update
 for (var i = 0; i < documentGroupItems.length; i++) {
@@ -14,26 +14,26 @@ function releaseClipGroups(groupItems) {
     var newGroup = app.activeDocument.groupItems.add()
     newGroup.name = "Piece " + (i + 1)
     for (var j = currentGroup.pageItems.length - 1; j >= 0; j--) {
-      // Count downwards to avoid issues with indexes
       var currentItem = currentGroup["pageItems"][j]
-      if (currentItem.strokeColor.typename != "NoColor") {
-        // Remove no stroke items
-        if (currentItem.strokeColor.blue != 255) {
-          // Remove blue lines
-          if (currentItem.strokeColor.blue == 90) {
-            // Sewing allowance to black
-            currentItem.strokeColor.blue = 0
-            currentItem.strokeColor.green = 0
-            currentItem.strokeColor.red = 0
-          }
-          currentItem.move(newGroup, ElementPlacement.PLACEATEND)
-          currentItem.strokeWidth = 2
-        } else {
-          currentItem.remove()
-        }
-      } else {
+      if (currentItem.strokeColor.typename == "NoColor") {
+        // Removing pathes without strokes (Clip paths, fabric color)
         currentItem.remove()
+        continue
       }
+      if (currentItem.strokeColor.blue == 255) {
+        // Removing blue lines
+        currentItem.remove()
+        continue
+      }
+      if (currentItem.strokeColor.blue == 90) {
+        // Changing stroke color of sewing allowance to black
+        currentItem.strokeColor.blue = 0
+        currentItem.strokeColor.green = 0
+        currentItem.strokeColor.red = 0
+      }
+      // Moving path to the new group
+      currentItem.move(newGroup, ElementPlacement.PLACEATEND)
+      currentItem.strokeWidth = NEW_STROKE_WIDTH
     }
   }
 }
