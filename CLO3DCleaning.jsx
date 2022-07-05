82fa631cd861c23adb402942a1a3ed3c14f048d0
alcpereira <48070464+alcpereira@users.noreply.github.com>
2022-07-05 17:54:27 +0100

    Update CLO3DCleaning.jsx
    Implemented duplicate notches removal

--- CLO3DCleaning.jsx
+++ CLO3DCleaning.jsx
@@ -1,18 +1,21 @@
+var NEW_STROKE_WIDTH = 1
+
 var documentGroupItems = app.activeDocument.groupItems
-const NEW_STROKE_WIDTH = 1
 var clipGroups = Array()
 // Separated array as `app.activeDocument.groupItems` will update
 for (var i = 0; i < documentGroupItems.length; i++) {
   clipGroups.push(documentGroupItems[i])
 }
 
-releaseClipGroups(clipGroups)
+cleanClipGroups(clipGroups)
 
-function releaseClipGroups(groupItems) {
+function cleanClipGroups(groupItems) {
   for (var i = 0; i < groupItems.length; i++) {
     var currentGroup = groupItems[i]
+    // Creating new groups to contain the paths
     var newGroup = app.activeDocument.groupItems.add()
     newGroup.name = "Piece " + (i + 1)
+    var visitedNotches = Array()
     for (var j = currentGroup.pageItems.length - 1; j >= 0; j--) {
       var currentItem = currentGroup["pageItems"][j]
       if (currentItem.strokeColor.typename == "NoColor") {
@@ -27,13 +30,34 @@ function releaseClipGroups(groupItems) {
       }
       if (currentItem.strokeColor.blue == 90) {
         // Changing stroke color of sewing allowance to black
+        // Idea: Join consecutive sewing allowance segments
         currentItem.strokeColor.blue = 0
         currentItem.strokeColor.green = 0
         currentItem.strokeColor.red = 0
+      }
+      if (currentItem.strokeColor.red == 255) {
+        // Remove duplicated notches
+        if (arrayIncludes(currentItem.position, visitedNotches)) {
+          currentItem.remove()
+          continue
+        } else {
+          visitedNotches.push(currentItem.position)
+        }
       }
       // Moving path to the new group
       currentItem.move(newGroup, ElementPlacement.PLACEATEND)
       currentItem.strokeWidth = NEW_STROKE_WIDTH
     }
+    // alert(visitedNotches)
   }
+}
+
+function arrayIncludes(value, array) {
+  // Custom function as ExtendScript doesn't have Array includes() functions
+  for (i = 0; i < array.length; i++) {
+    if (value[0] == array[i][0] && value[1] == array[i][1]) {
+      return true
+    }
+  }
+  return false
 }
